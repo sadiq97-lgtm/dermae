3223cae850bc763d28ce4e2b427bef91586e5e13
Sadiq Jaafar <seniorbaghdad7g@gmail.com>
2026-08-24 09:59:40 +0300

    Add routed premium product pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/*" element={<App />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
